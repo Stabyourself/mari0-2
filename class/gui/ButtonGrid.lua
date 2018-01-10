027aa2cc66e4b9de5d5626e47ca1ca1c29830df5
Maurice <maurice@guegan.de>
2018-01-10 20:56:00 +0100

    whoops

--- class/gui/ButtonGrid.lua
+++ class/gui/ButtonGrid.lua
@@ -1,8 +1,8 @@
 ButtonGrid = class("GUI.Button", GUI.Element)
 
 ButtonGrid.perRow = 73
-ButtonGrid.size = Vector(16, 16)
-ButtonGrid.gutter = Vector(1, 1)
+ButtonGrid.size = {16, 16}
+ButtonGrid.gutter = {1, 1}
 
 function ButtonGrid:initialize(x, y, img, buttons, func)
     self.img = img
@@ -20,14 +20,14 @@ function ButtonGrid:update(dt, x, y, mouseBlocked)
     
     local maxWidth = self.parent:getInnerWidth()
     
-    self.perRow = math.max(1, math.floor((maxWidth-self.x)/(self.size.x+self.gutter.x)))
+    self.perRow = math.max(1, math.floor((maxWidth-self.x)/(self.size[1]+self.gutter[1])))
     
     self:updateSize()
 end
 
 function ButtonGrid:updateSize()
-    self.w = self.perRow*(self.size.x+self.gutter.x)-self.gutter.x
-    self.h = math.ceil(#self.buttons/self.perRow*(self.size.y+self.gutter.y)-self.gutter.y)
+    self.w = self.perRow*(self.size[1]+self.gutter[1])-self.gutter[1]
+    self.h = math.ceil(#self.buttons/self.perRow*(self.size[2]+self.gutter[2])-self.gutter[2])
 end
 
 function ButtonGrid:getCollision(x, y)
@@ -35,8 +35,8 @@ function ButtonGrid:getCollision(x, y)
         return false
     end
     
-    local tileX = math.ceil(x/(self.size.x+self.gutter.x))
-    local tileY = math.ceil(y/(self.size.y+self.gutter.y))
+    local tileX = math.ceil(x/(self.size[1]+self.gutter[1]))
+    local tileY = math.ceil(y/(self.size[2]+self.gutter[2]))
     
     if tileX < 1 or tileX > self.perRow or
         tileY < 1 or tileY > math.ceil(#self.buttons/self.perRow) then
@@ -57,18 +57,18 @@ function ButtonGrid:draw(level)
         local tileX = (i-1)%self.perRow+1
         local tileY = math.ceil(i/self.perRow)-1
         
-        local topY = math.floor((self.parent.scroll[2]-self.y)/(self.size.y+self.gutter.y))
-        local bottomY = math.floor((self.parent.scroll[2]-self.y)/(self.size.y+self.gutter.y) + (self.parent:getInnerHeight())/(self.size.y+self.gutter.y))
+        local topY = math.floor((self.parent.scroll[2]-self.y)/(self.size[2]+self.gutter[2]))
+        local bottomY = math.floor((self.parent.scroll[2]-self.y)/(self.size[2]+self.gutter[2]) + (self.parent:getInnerHeight())/(self.size[2]+self.gutter[2]))
         
         if tileY >= topY and tileY <= bottomY then
-            local x = (tileX-1)*(self.size.x + self.gutter.x)
-            local y = tileY*(self.size.y + self.gutter.y)
+            local x = (tileX-1)*(self.size[1] + self.gutter[1])
+            local y = tileY*(self.size[2] + self.gutter[2])
             
             love.graphics.draw(self.img, self.buttons[i], x, y)
             
             if i == mouseTile then
                 love.graphics.setColor(1, 1, 1, 0.7)
-                love.graphics.rectangle("fill", x, y, self.size.x, self.size.y)
+                love.graphics.rectangle("fill", x, y, self.size[1], self.size[2])
                 love.graphics.setColor(1, 1, 1, 1)
             end
             
